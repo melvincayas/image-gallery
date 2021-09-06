6a768a9e780839b282dcd5f654fc37303b6aea53
Melvin Cayas <cayasmj@gmail.com>
2021-09-06 18:56:34 -0400

    Added JS doc into index.test.js

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,24 +1,30 @@
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
 import Home from "./index";
-import { expect, test } from "@jest/globals";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Function setup for shallow method on Home component
+ * @function setup
+ * @returns {ShallowWrapper} - Top level of component
+ */
 
 const setup = () => {
 	return shallow(<Home />);
 };
 
-describe("<Home /> Component", () => {
-	test("renders with no error", () => {
+describe("<Home /> Component renders properly", () => {
+	test("renders component with no error", () => {
 		const wrapper = setup();
 		const homeComponent = wrapper.find('[data-test="component-home"]');
 		expect(homeComponent.exists()).toBe(true);
 	});
 
-	test("renders the string Hello", () => {
-		const wrapper = setup();
-		const textComponent = wrapper.find('[data-test="string"]');
-		expect(textComponent.text()).toBe("Hello");
-	});
+	test("renders Navbar with no error", () => {});
+
+	test("renders Form with no error", () => {});
+
+	test('displays "Please enter something to search" text', () => {});
 });
