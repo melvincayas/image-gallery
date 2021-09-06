d8f4ae34b48ca87a50f865481f956ca4080f54c6
Melvin Cayas <cayasmj@gmail.com>
2021-09-06 18:56:49 -0400

    Add Navbar to App

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { Fragment } from "react";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<Navbar />
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
