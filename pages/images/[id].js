bbf9029eabe63e31ffc8d04c7908aa7b2fa38a68
Melvin Cayas <cayasmj@gmail.com>
2021-09-07 18:20:04 -0400

    Add pages/images and delete api

---
+++ pages/images/[id].js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/ImageDetail.module.css";
+
+const ImageDetail = () => {
+	return <div className={styles.container}>Image Detail Page</div>;
+};
+
+export default ImageDetail;
