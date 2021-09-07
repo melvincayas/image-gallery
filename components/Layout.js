abcfb9c4c2405abde8096ed0886f4f3beaf2f7c6
Melvin Cayas <cayasmj@gmail.com>
2021-09-07 19:35:10 -0400

    Remove fixed Navbar and margins

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,13 @@ import { Fragment } from "react";
 import PropTypes from "prop-types";
 
 import Navbar from "./Navbar";
+import styles from "./Layout.module.css";
 
 const Layout = props => {
 	return (
 		<Fragment>
 			<Navbar />
-			{props.children}
+			<main className={styles.container}>{props.children}</main>
 		</Fragment>
 	);
 };
