b7595186f42895b2b8b75eef19bb3201702e8642
Melvin Cayas <cayasmj@gmail.com>
2021-09-08 19:26:53 -0400

    Add Head into Layout.js for title

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,16 @@
 import { Fragment } from "react";
 import PropTypes from "prop-types";
 
+import Head from "next/head";
 import Navbar from "./Navbar";
 import styles from "./Layout.module.css";
 
 const Layout = props => {
 	return (
 		<Fragment>
+			<Head>
+				<title>Image Gallery</title>
+			</Head>
 			<Navbar />
 			<main className={styles.container}>{props.children}</main>
 		</Fragment>
